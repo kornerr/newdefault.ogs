a9742226b55ae5880cdab3169c92de02f29dc808
Michael Kapelko <kornerr@gmail.com>
2015-09-03 21:06:17 +0700

    #2297 Add Actions to Editor - Main script loads/unloads TrackNodeLifeTime script for selected nodes

--- scripts/TrackNodeLifeTime.py
+++ scripts/TrackNodeLifeTime.py
@@ -3,9 +3,18 @@ import pymjin2
 
 class TrackNodeLifeTime:
     def __init__(self, sceneName, nodeName, scene):
-        print "TrackNodeLifeTime.__init__. scene: {0} node: {1}".format(sceneName, nodeName)
+        print "TrackNodeLifeTime({0}). START. scene: {1} node: {2}".format(
+            id(self),
+            sceneName,
+            nodeName)
+        # Refer.
+        self.sceneName = sceneName
+        self.nodeName  = nodeName
     def __del__(self):
-        print "TrackNodeLifeTime.__del__"
+        print "TrackNodeLifeTime({0}). FINISH. scene: {1} node: {2}".format(
+            id(self),
+            self.sceneName,
+            self.nodeName)
 
 def SCRIPT_CREATE(sceneName, nodeName, scene):
     return TrackNodeLifeTime(sceneName, nodeName, scene)
